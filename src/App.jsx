bf86df3ea50d40d8d714f5dd52bd44ebc96355f3
Munzer Topu <munzertopu@gmail.com>
2025-08-28 22:25:47 +0600

    app.jsx and main.jsx integrated with router and redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import AdminLogin from "./components/auth/AdminLogin";
+import MemberLogin from "./components/auth/MemberLogin";
+import MerchantLogin from "./components/auth/MerchantLogin";
+import { restoreSession } from "./store/slices/authSlice";
+
+import AdminDashboard from "./components/dashboard/admin/AdminDashboard";
+import MemberDashboard from "./components/dashboard/member/MemberDashboard";
+import MerchantDashboard from "./components/dashboard/merchant/MerchantDashboard";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(restoreSession());
+  }, [dispatch]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Navigate to="/login/admin" replace />} />
+
+      <Route path="/login/admin" element={<AdminLogin />} />
+      <Route path="/login/merchant" element={<MerchantLogin />} />
+      <Route path="/login/member" element={<MemberLogin />} />
+      <Route
+        path="/dashboard/admin/*"
+        element={
+          <ProtectedRoute role="admin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/dashboard/merchant/*"
+        element={
+          <ProtectedRoute role="merchant">
+            <MerchantDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/dashboard/member/*"
+        element={
+          <ProtectedRoute role="member">
+            <MemberDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/login/admin" replace />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
